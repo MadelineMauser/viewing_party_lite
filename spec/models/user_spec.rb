809cdbb5b9f2506cc1232811faf52c00fc2681b5
Madeline Mauser <madeline.mauser@gmail.com>
2022-12-12 14:06:11 -0700

    Add password columns to User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email) }
+    it { should validate_presence_of(:password_digest) }
   end
 
   describe 'instance methods' do
@@ -56,5 +57,11 @@ RSpec.describe User, type: :model do
         expect(@user1.other_users).to eq([@user2, @user3, @user4])
       end
     end
+  end
+  describe 'authentication' do
+    it { should have_secure_password }
+    user = User.create(name: 'Meg', email: 'meg@test.com', password: 'password123', password_confirmation: 'password123')
+    expect(user).to_not have_attribute(:password)
+    expect(user.password_digest).to_not eq('password123')
   end
 end
