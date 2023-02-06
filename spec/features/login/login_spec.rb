706287535519654d796235a2beb852e7f9c712db
Madeline Mauser <madeline.mauser@gmail.com>
2023-02-06 13:19:45 -0700

    Add tests to login spec

--- spec/features/login/login_spec.rb
+++ spec/features/login/login_spec.rb
@@ -9,11 +9,22 @@ RSpec.describe 'Login' do
   describe 'login form' do
     it 'has a link on the home page', :vcr do
       visit '/'
+      save_and_open_page
       click_link 'Log In'
       expect(page).to have_current_path('/login')
     end
     it 'has a form to submit login information', :vcr do
-      visit "/users"
+      visit "/login"
+      expect(page).to have_field("Email")
+      expect(page).to have_field("Password")
+      expect(page).to have_button("Log In")
+    end
+    it 'logs the user in and redirects to the user dashboard' do
+      visit "/login"
+      fill_in "Email", with: @user1.email
+      fill_in "Password", with: @user1.password
+      click_button "Log In"
+      expect(page).to have_current_path(/dashboard)
       expect(page).to have_content(@user1.name)
     end
   end
